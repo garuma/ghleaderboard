4b0f43e442118d027ab25cb15f13678711458217
Jérémie Laval <jeremie.laval@gmail.com>
2014-06-19 23:43:22 -0400

    Add LoginDialogFragment

---
+++ GHLeaderboard/LoginDialogFragment.fs
@@ -0,0 +1,24 @@
+﻿
+namespace GHLeaderboard
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Text
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Util
+open Android.Views
+open Android.Widget
+
+type LoginDialogFragment() =
+  inherit DialogFragment ()
+
+  override x.OnCreate (savedInstanceState) =
+    base.OnCreate (savedInstanceState)
+    // Create your fragment here
+
+
